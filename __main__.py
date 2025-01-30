2d8be3f3d932b663f0d93e8cc740810ecc4de7a9
polirritmico <ebray187@gmail.com>
2025-01-30 11:53:27 -0300

    feat: replace main calls with higher-level scriptum functions

--- __main__.py
+++ __main__.py
@@ -1,39 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
+
 from src.scriptorium import Scriptorium
 
 
 def main():
-    from src.coqui import CoquiTTS
-    from src.importers.simple_text import SimpleTextImporter
-
-    input_files = [
-        "tests/files/test.txt",
-    ]
-    opts = {}
+    opts = {
+        "input": "source_text",  # dir or file
+        "input_handler": "SimpleTextImporter",
+        "transmuters": ["Translator"],
+        "output": "target_path",  # output dir
+        "title": "Archivo de prueba",
+        "creator": "Anónimo",
+        "lang": "es",
+    }
 
     scriptum = Scriptorium()
-    text_to_speech = CoquiTTS()
-    importer = SimpleTextImporter()
-
-    # TODO: It seems odd this approach, shouldn't be all defined in the opts?
-    # In that case, the scriptum should resolve this internally, so a higher
-    # level function is required...
-    # Input could be a list or a single file
-    for file in input_files:
-        local_opts = opts.copy()
-        local_opts["input_files"] = file
-
-        scriptum.set_importer(importer)
-        scriptum.set_transmuters(text_to_speech)
-        scriptum.set_options(local_opts)
-
-        scriptum.load_data()
-        scriptum.transmute()
-
-        scriptum.validate_output()
-        scriptum.export()
+    scriptum.set_options(opts)
+    scriptum.synthesize()
+    scriptum.crystallize()
 
 
 if __name__ == "__main__":
