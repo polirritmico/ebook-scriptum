be2620091d64d63c11186241f0e8c1df82f38b00
polirritmico <ebray187@gmail.com>
2025-01-30 11:56:33 -0300

    feat: update scriptum to use collector

--- src/scriptorium.py
+++ src/scriptorium.py
@@ -3,19 +3,22 @@
 
 from pathlib import Path
 
+from src.catalyst_collector import CatalystCollector
 from src.document import Document
 from src.protocols import ImporterHandler, TransmuterHandler
 
 
 class Scriptorium:
-    DEFAULT_OUTPUT: str = "output"
+    DEFAULT_OUTPUT_PATH: str = "output"
 
     def __init__(self):
         self.importer: ImporterHandler | None = None
         self.transmuters: TransmuterHandler | list[TransmuterHandler] | None = None
-        self.opts: dict = {}
+        self.options: dict = {}
         self.document: Document | None = None
         self.input_files: list[Path] | None = None
+
+        self.collector = CatalystCollector()
 
     def set_importer(self, importer: ImporterHandler) -> None:
         self.importer = importer
@@ -28,35 +31,57 @@ class Scriptorium:
         self.transmuters = transmuters
 
     def set_options(self, opts) -> None:
+        opts = self.collector.collect_options(opts)
         if not self.validate_options(opts):
             raise ValueError("Bad options")
-        self.opts = opts
+        parsed_opts = self.parse_options(opts)
+        self.options = parsed_opts
 
-    def validate_options(self, opts) -> bool:
+    def parse_options(self, opts: dict) -> dict:
+        collect_handler = self.collector.collect_handler
+        transmuters = []
+        for transmuter_name in opts.get("transmuters"):
+            transmuter = collect_handler(transmuter_name, "src.transmuters")
+            transmuters.append(transmuter)
+        importer = collect_handler(opts.get("importer"), "src.importers")
+        self.transmuters = transmuters
+        self.importer = importer
+
+        opts["input"] = Path(opts.get("input"))
+        opts["transmuters"] = transmuters
+        opts["importer"] = importer
+        return opts
+
+    def validate_options(self, opts: dict) -> bool:
         # TODO: options should be specific to the transmuter/model? If so they
         # need to be validated here. So we need to access those validations from
         # the setted transmuter.
         try:
-            input_files = self.opts.get("input_files")
+            input_files = opts.get("input")
             if isinstance(input_files, str):
                 input_files = [input_files]
             for i, file in enumerate(input_files):
                 input_files[i] = Path(file)
             self.input_files = input_files
 
-            output = self.opts.get("output", self.DEFAULT_OUTPUT)
+            output = self.options.get("output", self.DEFAULT_OUTPUT_PATH)
             if isinstance(output, str):
                 output = Path(output)
             elif not isinstance(output, Path):
                 raise TypeError("opts.output is not a string nor a Path")
-            self.output = output
+            # if not output.exists():
+            #     # create dir?
+            #     # create file?
+            #     pass
 
+            self.output = output
             return True
+
         except Exception:
             return False
 
     def load_data(self) -> Document:
-        source: Path = self.opts.get("input_file")
+        source: Path = self.options.get("input_file")
         self.importer.load_data(source)
         self.document = self.importer.generate_document()
         return self.document
