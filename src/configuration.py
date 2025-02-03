13c6e4f84f532f61b6acdb2a358ab54536139060
polirritmico <ebray187@gmail.com>
2025-02-03 20:56:21 -0300

    feat: added parse_opts to configuration module

--- src/configuration.py
+++ src/configuration.py
@@ -6,10 +6,17 @@ from pathlib import Path
 from src.catalyst_collector import CatalystCollector
 from src.protocols import ImporterHandler, TransmuterHandler
 
+IMPORTERS_SOURCE = "src.importers"
+MODELS_SOURCE = "src.modules"
+TRANSMUTERS_SOURCE = "src.transmuters"
 
-class ScriptoriumConfig:
+
+class ScriptoriumConfiguration:
     def __init__(self) -> None:
+        self.transmuters: list[TransmuterHandler] | None = None
+        self.importer = None
         self.validated = False
+        self.collector = CatalystCollector()
 
     def validate(self, opts: dict) -> bool:
         self.validated = True
@@ -21,7 +28,25 @@ class ScriptoriumConfig:
             raise ValueError("Bad options")
 
     def parse_options(self, opts: dict) -> dict:
-        pass
+        opts["input"] = Path(opts.get("input"))
+
+        collect_handler = self.collector.collect_handler
+        if not self.transmuters:
+            transmuters = []
+            for transmuter_name in opts.get("transmuters"):
+                Transmuter = collect_handler(transmuter_name, TRANSMUTERS_SOURCE)
+                transmuters.append(Transmuter())
+            if not transmuters:
+                raise ValueError("Not transmuter has been loaded")
+            self.transmuters = transmuters
+
+        if not self.importer:
+            Importer = collect_handler(opts.get("importer"), IMPORTERS_SOURCE)
+            if not Importer:
+                raise ValueError("Missing importer")
+            self.importer = Importer()
+
+        return opts
 
     def set_importer(self, importer: ImporterHandler | None = None) -> None:
         pass
