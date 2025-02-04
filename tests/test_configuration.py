9489fe8d34ebeb5061312712860eaca4ffb03f88
polirritmico <ebray187@gmail.com>
2025-02-03 23:29:03 -0300

    tests: add tests for configuration module

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,22 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import pytest
+
 from src.configuration import ScriptoriumConfiguration
+from src.models.llama3_2 import ModelLlama3_2
 from src.protocols import ImporterHandler, TransmuterHandler
+
+
+def test_minimal_keys_check_ok() -> None:
+    case = {
+        "input": "tests/files/simple_ebook.epub",
+        "output": "tests/files/output",
+        "transmuters": {"Translator": ""},
+        "importer": "EpubImporter",
+    }
+    config = ScriptoriumConfiguration()
+    config.check_minimun_required_keys(case)
+
+
+def test_minimal_keys_check_missing_input() -> None:
+    case = {
+        "output": "tests/files/output",
+        "transmuters": {"Translator": ""},
+        "importer": "EpubImporter",
+    }
+    with pytest.raises(KeyError) as error:
+        config = ScriptoriumConfiguration()
+        config.check_minimun_required_keys(case)
+
+    expected_error = "Missing parameter: 'input'"
+    output_error = error.value.args[0]
+    assert expected_error in output_error
 
 
 def test_parse_opts() -> None:
     case = {
         "input": "tests/files/simple_ebook.epub",
         "output": "tests/files/output",
-        "transmuters": ["Translator"],
+        "transmuters": {"Translator": ""},
         "importer": "EpubImporter",
     }
     ExpectedType = ImporterHandler
 
     config = ScriptoriumConfiguration()
-    config.parse_options(case)
+    config.parse_handlers(case)
 
-    OutputType = type(config.importer)
+    OutputType = config.importer
     assert config.importer
     assert isinstance(OutputType, ExpectedType)
+
+
+def test_parse_opts_model() -> None:
+    case = {
+        "input": "tests/files/simple_ebook.epub",
+        "output": "tests/files/output",
+        "transmuters": {"Translator": "ModelLlama3_2"},
+        "importer": "EpubImporter",
+    }
+    ExpectedType = ModelLlama3_2
+
+    config = ScriptoriumConfiguration()
+    config.parse_handlers(case)
+    assert len(config.transmuters) == 1
+    first_transmuter_with_model = config.transmuters[0]
+    Model = first_transmuter_with_model[1]
+    assert Model is ExpectedType
