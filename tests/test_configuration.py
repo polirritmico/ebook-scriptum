2efc6d0d1558f3c38da7f93079d4666f04aae058
polirritmico <ebray187@gmail.com>
2025-02-04 21:36:16 -0300

    tests: add tests for new configuration module capabilities

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -4,8 +4,55 @@
 import pytest
 
 from src.configuration import ScriptoriumConfiguration
+from src.importers.simple_text import SimpleTextImporter
 from src.models.llama3_2 import ModelLlama3_2
-from src.protocols import ImporterHandler, TransmuterHandler
+from src.protocols import ImporterHandler, ModelHandler
+from src.transmuters.translator import Translator
+
+
+def test_transmuter_and_importer_not_overwriten_by_config() -> None:
+    case = {
+        "input": "tests/files/simple_ebook.epub",
+        "output": "tests/files/mock.epub",
+        "transmuters": {"Translator": ""},
+        "importer": "EpubImporter",
+    }
+
+    case_importer = SimpleTextImporter()
+    case_transmuter = Translator()
+    case_model = ModelLlama3_2()
+    case_transmuter.set_model(case_model)
+
+    config = ScriptoriumConfiguration()
+    config.importer = case_importer
+    config.transmuters = [case_transmuter]
+
+    config.set_options(case)
+    config.parse_options(case)
+    out_transmuter = config.transmuters[0]
+
+    assert case_model is out_transmuter.model
+    assert case_transmuter is out_transmuter
+    assert case_importer is config.importer
+
+
+def test_load_config_json() -> None:
+    case = "tests/files/config.json"
+    expected_lang = "es"
+    expected_input = "tests/files/simple.txt"
+    ExpectedImporter = SimpleTextImporter
+    ExpectedTransmuter = Translator
+    ExpectedModel = ModelHandler
+
+    config = ScriptoriumConfiguration()
+    case = config.set_options(case)
+    config.parse_options(case)
+
+    assert expected_input == config.input_file.as_posix()
+    assert expected_lang == config.metadata["lang"]
+    assert isinstance(config.importer, ExpectedImporter)
+    assert isinstance(config.transmuters[0], ExpectedTransmuter)
+    assert isinstance(config.transmuters[0].model, ExpectedModel)
 
 
 def test_minimal_keys_check_ok() -> None:
@@ -16,7 +63,7 @@ def test_minimal_keys_check_ok() -> None:
         "importer": "EpubImporter",
     }
     config = ScriptoriumConfiguration()
-    config.check_minimun_required_keys(case)
+    config.check_spec_compliance(case)
 
 
 def test_minimal_keys_check_missing_input() -> None:
@@ -25,13 +72,32 @@ def test_minimal_keys_check_missing_input() -> None:
         "transmuters": {"Translator": ""},
         "importer": "EpubImporter",
     }
-    with pytest.raises(KeyError) as error:
+    with pytest.raises(ValueError) as error:
         config = ScriptoriumConfiguration()
-        config.check_minimun_required_keys(case)
+        config.setup(case)
 
-    expected_error = "Missing parameter: 'input'"
+    expected = ("missing", "input")
     output_error = error.value.args[0]
-    assert expected_error in output_error
+    for expected_str in expected:
+        assert expected_str in output_error.lower()
+
+
+def test_detect_opts_mismatch_types_with_spec() -> None:
+    case = {
+        "input": "tests/files/simple_ebook.epub",
+        "output": "tests/files/output",
+        "transmuters": {"Translator": ""},
+        "importer": ["EpubImporter"],  # this should not be a list
+    }
+    expected = ["type", "importer", "list", "str"]
+
+    with pytest.raises(ValueError) as error:
+        config = ScriptoriumConfiguration()
+        config.check_spec_compliance(case)
+
+    output_error = error.value.args[0]
+    for expected_str in expected:
+        assert expected_str in output_error.lower()
 
 
 def test_parse_opts() -> None:
@@ -62,7 +128,9 @@ def test_parse_opts_model() -> None:
 
     config = ScriptoriumConfiguration()
     config.parse_handlers(case)
-    assert len(config.transmuters) == 1
-    first_transmuter_with_model = config.transmuters[0]
+
+    assert config.transmuters_types
+    assert len(config.transmuters_types) == 1
+    first_transmuter_with_model = config.transmuters_types[0]
     Model = first_transmuter_with_model[1]
     assert Model is ExpectedType
