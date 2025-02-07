cfaead7e5f43e49e900028eb754b0a64d04aa7ce
polirritmico <ebray187@gmail.com>
2025-02-07 18:18:26 -0300

    feat: use tag.string to get translatable text objects in the soup

--- src/transmuters/translator.py
+++ src/transmuters/translator.py
@@ -30,6 +30,18 @@ class Translator:
         self.response = None
         self.run_validator = None
         self.max_retry_attemps = 10
+        self.translatable_tags = [
+            "p",
+            "h1",
+            "h2",
+            "h3",
+            "h4",
+            "h5",
+            "h6",
+            "li",
+            "a",
+            "span",
+        ]
 
     def set_model(self, model: ModelHandler | None) -> None:
         if model and model.transmuter_type != self.transmuter_type:
@@ -48,12 +60,6 @@ class Translator:
             self.translate_section_metadata(section)
             self.translate_section_content(section)
 
-    def check_ollama(self) -> None:
-        try:
-            ollama_list()
-        except Exception:
-            raise
-
     def translate_section_metadata(self, section: Section) -> None:
         section.title = self.translate_metadata(section.title)
 
@@ -65,10 +71,10 @@ class Translator:
         self.translated_metadata[text] = translated_text
 
     def translate_section_content(self, section: Section) -> str:
-        for tag in section.content:
-            translated_text = self.translate_text(section.text)
-            section.text = translated_text
-            # section.update_tag(translated_text, tag)
+        for tag in section.content.find_all(self.translatable_tags):
+            if tag.string:
+                translated_text = self.translate_text(tag.string)
+                tag.string.replace_with(translated_text)
 
     def translate_text(self, text: str) -> str:
         attempts = 0
@@ -79,6 +85,12 @@ class Translator:
             attempts += 1
             if attempts == self.max_retry_attemps:
                 return response
+
+    def check_ollama(self) -> None:
+        try:
+            ollama_list()
+        except Exception:
+            raise
 
     def send_prompt(self, text_to_translate: str = None) -> str:
         msg = self.model.make_instructions(text_to_translate)
