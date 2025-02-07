b64cc296d485e2865dcf8f504f38578ed1d56a10
polirritmico <ebray187@gmail.com>
2025-02-07 18:04:06 -0300

    feat: move source_path to source in Document class

--- src/document.py
+++ src/document.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
+
 from src.dataclass import DocumentMetadata, Section
 
 
@@ -8,11 +10,12 @@ class Document:
     def __init__(self):
         self.metadata: DocumentMetadata | None = None
         self.sections: dict[str, Section] | None = None
-        self.source_path: str = ""
+        self.source: Path | None = None
 
     def set_medatada(self, metadata: DocumentMetadata) -> None:
         self.validate_document_metadata(metadata)
         self.metadata = metadata
+        self.source = metadata.source
 
     def set_sections(self, sections: dict[str, Section]) -> None:
         self.validate_sections(sections)
