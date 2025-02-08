f657240037d617f690eca46ead5f1621e05bd63a
polirritmico <ebray187@gmail.com>
2025-02-08 19:20:13 -0300

    feat: update epub_exporter into a functional state

--- src/exporters/epub.py
+++ src/exporters/epub.py
@@ -32,41 +32,23 @@ class EpubExporter:
         else:
             self.export_new_epub(document, output)
 
-    # def split_dir_and_file(self, path: Path | None = None) -> (Path, Path):
-    #     if not path:
-    #         raise ValueError("Empty path")
-    #
-    #     if path.is_dir():
-    #         dir = path
-    #         file = Path(self.DEFAULT_OUTPUT_FILENAME)
-    #     else:
-    #         dir = path.parent
-    #         file = path
-    #
-    #     return (dir, file)
-
     def export_with_base_document(self, document: Document, output: Path) -> None:
-        # self.update_epub_metadata()
         with TemporaryDirectory(prefix=self.tmp_prefix) as temp_path_str:
             tmp_path = Path(temp_path_str)
             self.extract_epub(document.source, tmp_path)
+            # self.update_epub_metadata()
             self.update_epub_section_files(document, tmp_path)
-            self.write_epub(document, tmp_path)
+            self.write_epub(output, tmp_path)
 
     def update_epub_section_files(self, document: Document, tmp_dir: Path) -> None:
         for file, section in document.sections.items():
             new_content = document.get_content(file)
-            target_path = self.unziped_target_path(section.filepath, tmp_dir)
+            target_path = tmp_dir / section.filepath
             try:
                 with open(target_path, "w", encoding="utf-8") as stream:
                     stream.write(new_content)
             except Exception:
                 raise Exception(f"Error writing the file '{file}'")
-
-    def unziped_target_path(self, section: Path, zip_dir: Path) -> Path:
-        old_tmp_dir_root = section.parts.index("OEBPS")
-        target_path = zip_dir / Path(*section.parts[old_tmp_dir_root:])
-        return target_path
 
     def write_epub(self, output: Path, source_dir: Path) -> None:
         if not any(source_dir.iterdir()):
