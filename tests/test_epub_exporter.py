3d0a8e252a1793a69b6b532014ce8bb1f2503e52
polirritmico <ebray187@gmail.com>
2025-02-08 19:24:26 -0300

    tests: assert against the output file instead the input

--- tests/test_epub_exporter.py
+++ tests/test_epub_exporter.py
@@ -9,7 +9,7 @@ from src.importers.epub import EpubImporter
 
 def test_export_epub() -> None:
     case = Path("tests/files/simple_ebook.epub")
-    output_path = "tests/files/outputs/exporter-export_epub"
+    output_path = "tests/files/outputs/test_export_epub"
 
     importer = EpubImporter()
     exporter = EpubExporter()
@@ -22,7 +22,7 @@ def test_update_epub_section() -> None:
     case = "THIS IS WORKING"
     expected = "THIS IS WORKING"
     case_input = Path("tests/files/simple_ebook.epub")
-    case_output = Path("tests/files/temp-test_update_epub_section.epub")
+    case_output = Path("tests/files/outputs/test_update_epub_section.epub")
 
     prev_importer = EpubImporter()
     prev_importer.load_data(case_input)
@@ -35,9 +35,9 @@ def test_update_epub_section() -> None:
     exporter.export(prev_document, case_output)
 
     after_importer = EpubImporter()
-    after_importer.load_data(case_input)
+    after_importer.load_data(case_output)
     after_document = after_importer.generate_document()
     after_section = after_document.sections["Section0001.xhtml"]
     output = after_section.content.find("p").string
 
-    assert expected == output
+    assert expected in output
