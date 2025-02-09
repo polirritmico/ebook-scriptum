b7a010538f66c91e91ae6328b76aed5851408399
polirritmico <ebray187@gmail.com>
2025-02-08 23:14:08 -0300

    tests: add simple text exporter test

---
+++ tests/test_simple_text_exporter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from pathlib import Path
+
+from src.scriptorium import Scriptorium
+
+
+def test_simple_text_export() -> None:
+    case_output_path = Path("tests/files/outputs/test_simple_text_export.txt")
+    case = {
+        "input": "tests/files/simple-translation.txt",
+        "output": case_output_path,
+        "transmuters": {"Translator": ""},
+        "importer": "SimpleTextImporter",
+        "exporter": "SimpleTextExporter",
+    }
+    expected = "domingo"
+
+    scriptum = Scriptorium()
+    scriptum.setup(case)
+    scriptum.load_data()
+    scriptum.transmute()
+    scriptum.export()
+    output = case_output_path.read_text()
+
+    assert expected in output.lower()
