5659f2de2a3215856dd91897cc3584d1ae0ae33e
polirritmico <ebray187@gmail.com>
2025-02-10 15:49:58 -0300

    fix: Move torch check into the __init__ to allow collector

--- src/transmuters/tts.py
+++ src/transmuters/tts.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 from src.document import Document
 from src.protocols import ModelHandler, TransmuterType
 
 
 class TtsTransmuter:
     transmuter_type: TransmuterType = TransmuterType.TTS
+
+    def __init__(self):
+        try:
+            global torch
+            import torch
+
+            assert torch.cuda.is_available()
+        except Exception:
+            raise ImportError
 
     def generic_response_validator(self, response: str | None, original: str) -> bool:
         return True
