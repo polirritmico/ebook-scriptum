c12cb1d4dba5c64264c350b9ba3c06ad2f72074e
polirritmico <ebray187@gmail.com>
2025-02-27 22:42:21 -0300

    feat: update dummy transmuter to new protocol

--- src/transmuters/dummy.py
+++ src/transmuters/dummy.py
@@ -2,17 +2,28 @@
 # -*- coding: utf-8 -*-
 
 
+from pathlib import Path
+
 from src.document import Document
-from src.protocols import ModelHandler, TransmuterType
+from src.protocols import ExporterHandler, ModelHandler, TransmuterType
 
 
 class DummyTransmuter:
     transmuter_type: TransmuterType = TransmuterType.LLM
 
+    def set_options(self, options: dict) -> None:
+        return
+
     def set_model(self, model: ModelHandler) -> None:
         return
 
+    def set_exporter(self, exporter: ExporterHandler) -> None:
+        return
+
     def transmute(self, document: Document) -> None:
+        return
+
+    def export(self, path: Path) -> None:
         return
 
     def generic_response_validator(self, foo, bar) -> bool:
