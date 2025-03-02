65da37d3ecb7903a54cf9150f8f7a4244f346ccd
polirritmico <ebray187@gmail.com>
2025-03-01 21:23:57 -0300

    feat: rename module file and implemente prepare_request function

--- src/models/vitts_es.py
+++ src/models/vitts_es.py
@@ -7,7 +7,11 @@ from typing import Any
 from src.protocols import TransmuterType
 
 
-class ModelVitts:
+class ModelVittsEs:
+    DEFAULT_VOCODER = "vocoder_models/universal/libri-tts/wavegrad"
+    DEFAULT_MODEL_NAME = "tts_models/es/css10/vits"
+    DEFAULT_LANG = "es"
+
     transmuter_type: TransmuterType = TransmuterType.TTS
 
     id: str = "vitts:latest"  # :14b or latest
@@ -17,10 +21,21 @@ class ModelVitts:
     def __init__(self, id: str | None = None, instruction: str | None = None):
         self.id = id if id else self.id
         self.instruction = instruction if instruction else self.base_instruction
-        self.name = "tts_models/es/css10/vits"
 
     def prepare_request(self, opts: dict) -> Any:
-        pass
+        opts = opts.get("vitts", opts)
+
+        self.vocoder = opts.get("vocoder", self.DEFAULT_VOCODER)
+        self.name = opts.get("name", self.DEFAULT_MODEL_NAME)
+        self.lang = opts.get("lang", self.DEFAULT_LANG)
+        self.speaker = opts.get("speaker", "")
+
+        return {
+            "vocoder_name": self.vocoder,
+            "name": self.name,
+            "lang": self.lang,
+            "speaker_wav": self.speaker,
+        }
 
     def response_validator(self, response: str | None, original: str) -> bool:
         return True
