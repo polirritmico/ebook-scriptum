7e19cf283cd71010abc70cfdd351d0e3d19026b7
polirritmico <ebray187@gmail.com>
2025-03-01 21:24:59 -0300

    feat: implement coqui_tts base functionality

--- src/transmuters/coqui_tts.py
+++ src/transmuters/coqui_tts.py
@@ -5,12 +5,14 @@
 from pathlib import Path
 
 from src.document import Document
-from src.models.vitts import ModelVitts as DefaultModel
-from src.protocols import ModelHandler, TransmuterType
+from src.models.vitts_es import ModelVittsEs as DefaultModel
+from src.protocols import ExporterHandler, ModelHandler, TransmuterType
+from src.utils.vitts_text_processor import VittsTextProcessor
 
 
 class CoquiTTS:
     transmuter_type: TransmuterType = TransmuterType.TTS
+    exporter: ExporterHandler | None
 
     def __init__(self):
         try:
@@ -27,88 +29,79 @@ class CoquiTTS:
 
         self.tts = None
         self.device = "cuda"
+        self.exporter = None
         self.opts = {}
 
     def generic_response_validator(self, response: str | None, original: str) -> bool:
         return True
 
+    def set_options(self, options: dict) -> None:
+        if not isinstance(options, dict):
+            raise TypeError("CoquiTTS bad options type")
+        self.opts = options
+
     def set_model(self, model: ModelHandler) -> None:
         if model and model.transmuter_type != self.transmuter_type:
             msg = f"ModelHandler {model.transmuter_type} incompatible with {self.transmuter_type}"
             raise TypeError(msg)
+        if "vitts" not in model.id:
+            raise ValueError(f"Model {model.id} is not a vitts model")
+
         self.model = model or DefaultModel()
         if self.model.response_validator:
             self.run_validator = self.model.response_validator
         else:
             self.run_validator = self.generic_response_validator
 
-    def process_text(self, document: Document) -> dict[Path, str]:
-        processed_document: dict[Path, str] = {}
-        return processed_document
+    def set_exporter(self, exporter: ExporterHandler) -> None:
+        if not isinstance(exporter, ExporterHandler):
+            raise TypeError("exporter is not an ExporterHandler")
+        self.exporter = exporter
 
     def transmute(self, document: Document) -> None:
-        # processed_document = self.process_text(document)
-        self.opts["vocoder"] = "vocoder_models/universal/libri-tts/wavegrad"
-        self.output_path = "tests/files/outputs/tts_out.wav"
+        processed_document = self.process_text(document)
+        self.document = processed_document
 
+        self.model.prepare_request(self.opts)
         self.tts = TTS(
             model_name=self.model.name, config_path=self.opts.get("config_path")
         ).to(self.device)
 
-        text = document.get_content("simple.txt", raw=True)
+    def export(self, output_path: Path) -> None:
+        if not hasattr(self, "document") or not hasattr(self, "tts"):
+            raise ValueError("Not transmuted document. Try transmute() first")
 
-        tts_opts = {
-            "file_path": self.output_path,
-            "text": text,
-            "speaker_wav": self.opts.get("speaker"),
-            "vocoder_name": self.opts.get("vocoder"),
-        }
+        tts_opts = {"vocoder_name": self.model.vocoder}
 
-        if hasattr(self, "lang_model") and self.lang_model:
-            tts_opts["lang"] = self.lang_model
+        # if hasattr(self, "lang_model") and self.lang_model:
+        #     tts_opts["lang"] = self.lang_model
 
-        self.tts.tts_to_file(**tts_opts)
-        return self.output_path
+        if len(self.document) > 1:
+            # FIX: Implement this and clean options! check if is an output dir
+            raise NotImplementedError("No multiple output files implemented")
 
-    # def process_file(file_path: str, output_file: str, opts):
-    #     print("opening text file...")
-    #     with open(file_path, "r", encoding="utf-8") as stream:
-    #         content = stream.read()
-    #
-    #     content = text_processor(content, word_dict, opts)
-    #
-    #     print("Executing text to audio...")
-    #     text_to_audio(content, output_file, opts)
+        # NOTE:
+        # 1. Change config 20100 -> 19800 khz to improve the tempo (to fast):
+        #    /home/docker-usr/.local/share/tts/tts_models--es--css10--vits/config.json
+        # 2. pysbd is hardcoded to `en` so changed to `es`:
+        #    ~/.local/lib/python3.10/site-packages/TTS/utils/synthesizer.py:93
 
-    # def main():
-    #     opts = {}
-    #     tag = ""
-    #
-    #     # Universal ES
-    #     opts["model"] = "tts_models/es/css10/vits"
-    #     opts["vocoder"] = "vocoder_models/universal/libri-tts/wavegrad"
-    #     opts["lang"] = "es"
-    #     opts["lang_model"] = False
-    #     # opts["vocoder"] = "vocoder_models/universal/libri-tts/fullband-melgan"
-    #
-    #     # NOTE: pysbd is hardcoded to `en` so changed to `es`:
-    #     #    ~/.local/lib/python3.10/site-packages/TTS/utils/synthesizer.py:93
-    #
-    #     # EN (apt install espeak-ng)
-    #     # opts["model"] = "tts_models/en/ljspeech/vits"
-    #     # opts["vocoder"] = "vocoder_models/en/ljspeech/hifigan_v2"
-    #     # opts["lang"] = "en"
-    #     # opts["lang_model"] = False
-    #
-    #     files = [
-    #         "path/to/section_file.txt",
-    #     ]
-    #
-    #     outdir = Path("output")
-    #     outdir.mkdir(exist_ok=True)
-    #     for file in files:
-    #         print(f"Processing file {file}")
-    #         file_name = f"{Path(file).stem}_{Path(opts.get('model')).name}{tag}.wav"
-    #         output_file = outdir / file_name
-    #         process_file(file, output_file, opts)
-    #         print("Done!")
+        for name, section in self.document.items():
+            tts_opts["text"] = section
+            tts_opts["file_path"] = output_path  # / name
+            self.tts.tts_to_file(**tts_opts)
+
+        return output_path
+
+    def process_text(self, document: Document) -> dict[Path, str]:
+        processed_document: dict[Path, str] = {}
+        opts = getattr(self, "opts", {})
+        book_words = opts.get("book_words", {})
+
+        process_text = VittsTextProcessor().process_text
+        for section_name, section in document.sections.items():
+            section_txt = document.get_content(section_name, True)
+            processed_text = process_text(section_txt, book_words, opts)
+            processed_document[section_name] = processed_text
+
+        return processed_document
