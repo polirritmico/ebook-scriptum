8fb042509717e02b189dcf0ab48adac15bbe038f
polirritmico <ebray187@gmail.com>
2025-03-02 22:08:58 -0300

    tests: update tts model

--- tests/test_tts.py
+++ tests/test_tts.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import pytest
 
 from src.importers.simple_text import SimpleTextImporter
-from src.models.vitts import ModelVitts
+from src.models.vitts_es import ModelVittsEs
 from src.transmuters.coqui_tts import CoquiTTS
 
 
@@ -15,17 +15,25 @@ from src.transmuters.coqui_tts import CoquiTTS
 def test_foo() -> None:
     case_file = Path("tests/files/simple.txt")
     output_file = Path("tests/files/outputs/tts_out.wav")
+    case_opts = {
+        "vitts": {
+            "lang": "es",
+            "log": None,
+        },
+        "output_file": output_file,
+    }
     output_file.unlink(missing_ok=True)  # clean old execution output
 
     text = SimpleTextImporter()
     text.load_data(case_file)
     doc = text.generate_document()
+
     tts = CoquiTTS()
-
-    model = ModelVitts()
-
+    model = ModelVittsEs()
     tts.set_model(model)
+    tts.set_options(case_opts)
     tts.transmute(doc)
+    tts.export(output_file)
 
     assert output_file.exists()
     assert output_file.is_file()
