0199ea04def4611b94645dce6729e5ea8a5ae6e4
polirritmico <ebray187@gmail.com>
2025-03-05 04:26:57 -0300

    feat: update imports and export func of coqui_tts

--- src/transmuters/coqui_tts.py
+++ src/transmuters/coqui_tts.py
@@ -6,8 +6,9 @@ from pathlib import Path
 
 from src.document import Document
 from src.models.vitts_es import ModelVittsEs as DefaultModel
+from src.processors.vitts_audio_processor import VittsAudioProcessor
+from src.processors.vitts_text_processor import VittsTextProcessor
 from src.protocols import ExporterHandler, ModelHandler, TransmuterType
-from src.utils.vitts_text_processor import VittsTextProcessor
 
 
 class CoquiTTS:
@@ -69,41 +70,61 @@ class CoquiTTS:
 
     def transmute(self, document: Document) -> None:
         processed_document = self.process_text(document)
-        self.document = processed_document
+        self.processed_document = processed_document
 
         self.model.prepare_request(self.opts)
         self.tts = TTS(
             model_name=self.model.name, config_path=self.opts.get("config_path")
         ).to(self.device)
 
-    def export(self, output_path: Path) -> None:
-        if not hasattr(self, "document") or not hasattr(self, "tts"):
+    def export(self, output_dir: Path) -> None:
+        # TODO: Add a document selector
+        if not hasattr(self, "processed_document") or not hasattr(self, "tts"):
             raise ValueError("Not transmuted document. Try transmute() first")
 
-        if len(self.document) > 1:
-            # FIX: Implement this and clean options! check if is an output dir
-            raise NotImplementedError("No multiple output files implemented")
+        # # TODO: REMOVE
+        # if len(self.document) > 1:
+        #     # FIX: Implement this and clean options! check if is an output dir
+        #     raise NotImplementedError("No multiple output files implemented")
 
         # NOTE:
         # 1. Change config 20100 -> 19800 khz to improve the tempo (to fast):
-        #    /home/docker-usr/.local/share/tts/tts_models--es--css10--vits/config.json
+        #    ~/.local/share/tts/tts_models--es--css10--vits/config.json
         # 2. pysbd is hardcoded to `en` so changed to `es`:
         #    ~/.local/lib/python3.10/site-packages/TTS/utils/synthesizer.py:93
+        #    .venv/lib/python3.12/site-packages/TTS/utils/synthesizer.py:93
 
-        for name, section in self.document.items():
-            self.tts.tts_to_file(**self.tts_opts, text=section, file_path=output_path)
+        if output_dir.is_file():
+            raise ValueError(f"Output path is not a dir: '{output_dir}'")
+        output_dir.mkdir(parents=True, exist_ok=True)
 
-        return output_path
+        processed_files = []
+        for name, section in self.processed_document.items():
+            if not section:
+                continue
+            name = Path(name).with_suffix(".wav")
+            output_file = output_dir / name
+            processed_files.append(output_file)
+            self.tts.tts_to_file(**self.tts_opts, text=section, file_path=output_file)
+
+        self.apply_audio_post_processing(processed_files)
+
+        return output_dir
+
+    def apply_audio_post_processing(self, files: list[Path]) -> None:
+        processor = VittsAudioProcessor()
+        for file in files:
+            processor.run(file)
 
     def process_text(self, document: Document) -> dict[Path, str]:
         processed_document: dict[Path, str] = {}
-        opts = getattr(self, "opts", {})
-        book_words = opts.get("book_words", {})
+        opts = self.opts.get("text_processor_opts") or self.model.text_processor_opts
+        book_words = self.opts.get("book_words", {})
 
-        process_text = VittsTextProcessor().process_text
+        text_processor = VittsTextProcessor().process_text
         for section_name, section in document.sections.items():
             section_txt = document.get_content(section_name, True)
-            processed_text = process_text(section_txt, book_words, opts)
+            processed_text = text_processor(section_txt, book_words, opts)
             processed_document[section_name] = processed_text
 
         return processed_document
