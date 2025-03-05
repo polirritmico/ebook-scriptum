8eab1bdd3b28c587e0cb57ccfc71e12a33b7dce6
polirritmico <ebray187@gmail.com>
2025-03-05 04:12:55 -0300

    feat: add default txt processor options to vitts es model

--- src/models/vitts_es.py
+++ src/models/vitts_es.py
@@ -11,8 +11,13 @@ class ModelVittsEs:
     DEFAULT_VOCODER = "vocoder_models/universal/libri-tts/wavegrad"
     DEFAULT_MODEL_NAME = "tts_models/es/css10/vits"
     DEFAULT_LANG = "es"
+    DEFAULT_TXT_PROC = {
+        "lang": DEFAULT_LANG,
+        "log": False,
+    }
 
     transmuter_type: TransmuterType = TransmuterType.TTS
+    response_validator = None
 
     id: str = "vitts:latest"  # :14b or latest
     base_instruction: dict = {}
@@ -21,6 +26,7 @@ class ModelVittsEs:
     def __init__(self, id: str | None = None, instruction: str | None = None):
         self.id = id if id else self.id
         self.instruction = instruction if instruction else self.base_instruction
+        self.text_processor_opts = self.DEFAULT_TXT_PROC
 
     def prepare_request(self, opts: dict) -> Any:
         opts = opts.get("vitts", opts)
@@ -30,12 +36,13 @@ class ModelVittsEs:
         self.lang = opts.get("lang", self.DEFAULT_LANG)
         self.speaker = opts.get("speaker", "")
 
+        text_processor_opts = opts.get("text_processor")
+        if text_processor_opts:
+            self.text_processor_opts = text_processor_opts
+
         return {
             "vocoder_name": self.vocoder,
             "name": self.name,
             "lang": self.lang,
             "speaker_wav": self.speaker,
         }
-
-    def response_validator(self, response: str | None, original: str) -> bool:
-        return True
