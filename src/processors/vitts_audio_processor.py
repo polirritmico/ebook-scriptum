73b9a4d3dff315b01848e841852754e5904c332b
polirritmico <ebray187@gmail.com>
2025-03-05 04:07:29 -0300

    feat: Add remove_inner_silences audio process

--- src/processors/vitts_audio_processor.py
+++ src/processors/vitts_audio_processor.py
@@ -7,11 +7,29 @@ import ffmpeg
 
 
 class AudioProcessor:
-    def remove_inner_silences(self, wav_file: str | Path) -> str:
-        pass
+    DEFAULT_INNER_SILENCE_FILTER = {
+        "stop_periods": -1,
+        "stop_threshold": "-40dB",
+        "stop_duration": 0.8,
 
-    def add_wrap_silences(self, wav_file: str | Path) -> str:
-        pass
+    def __init__(self):
+        self.tmp_register: list[str] = []
 
-    def wav_to_mp3(self, wav_file: str, output_file: str, settings: dict) -> None:
-        pass
+
+    def remove_inner_silences(
+        self, wav_file: str | Path, filter: dict | None = None
+    ) -> str:
+        if isinstance(wav_file, Path):
+            wav_file = str(wav_file)
+        tmp_file = self.make_temp_filename(wav_file)
+        filter = filter or self.DEFAULT_INNER_SILENCE_FILTER
+
+        stream = ffmpeg.input(wav_file).filter("silenceremove", **filter)
+        stream.output(tmp_file).run(overwrite_output=True)
+        return tmp_file
+
+    def make_temp_filename(self, input_file: str) -> str:
+        tmp_idx = len(self.tmp_register) + 1
+        tmp_filename = f"{input_file[:-4]}_temp{tmp_idx}.wav"
+        self.tmp_register.append(tmp_filename)
+        return tmp_filename
