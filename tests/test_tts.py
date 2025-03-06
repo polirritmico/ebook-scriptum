beba5f87b4fa59efa24da1b89ffae0b23d1f2c88
polirritmico <ebray187@gmail.com>
2025-03-06 19:37:19 -0300

    tests(tts_acceptance): use tmp_dir and update asserts

--- tests/test_tts.py
+++ tests/test_tts.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import pytest
 
@@ -10,39 +11,34 @@ from src.models.vitts_es import ModelVittsEs
 from src.transmuters.coqui_tts import CoquiTTS
 
 
+@pytest.fixture
+def tmp_dir():
+    with TemporaryDirectory(dir="tests/files/", prefix="tts-test-") as tmp:
+        yield Path(tmp)
+
+
 @pytest.mark.filterwarnings("ignore::DeprecationWarning")
 @pytest.mark.filterwarnings("ignore::UserWarning")
-def test_tts_acceptance() -> None:
+def test_tts_acceptance(tmp_dir) -> None:
     case_file = Path("tests/files/simple.txt")
-    output_dir = Path("tests/files/outputs/audios")
-    outfile = output_dir / "simple.wav"
-    outfile.unlink(missing_ok=True)  # clean old execution output
-    outfile.with_suffix(".mp3").unlink(missing_ok=True)
-
-    case_opts = {
-        "vitts": {
-            "lang": "es",
-            "log": True,
-        },
-        # TODO: is this used? change to output_dir?
-        "output_file": output_dir,
-    }
+    expected1 = tmp_dir / "simple.wav"
+    expected2 = tmp_dir / "simple.mp3"
+    case_opts = {"lang": "es", "log": True, "keep_wav": True}
 
     text = SimpleTextImporter()
     text.load_data(case_file)
-    doc = text.generate_document()
+    document = text.generate_document()
 
     tts = CoquiTTS()
     model = ModelVittsEs()
     tts.set_model(model)
     tts.set_options(case_opts)
-    tts.transmute(doc)
-    tts.export(output_dir)
+    tts.transmute(document)
+    tts.export(tmp_dir)
 
-    assert outfile.exists()
-    assert outfile.is_file()
-    assert outfile.stat().st_size > 0, "mp3 file should not be an empty file"
-    outfile.with_suffix(".mp3")
-    assert outfile.exists()
-    assert outfile.is_file()
-    assert outfile.stat().st_size > 0, "wav file should not be an empty file"
+    assert expected1.exists()
+    assert expected1.is_file()
+    assert expected1.stat().st_size > 0, "wav file should not be an empty file"
+    assert expected2.exists()
+    assert expected2.is_file()
+    assert expected2.stat().st_size > 0, "mp3 file should not be an empty file"
