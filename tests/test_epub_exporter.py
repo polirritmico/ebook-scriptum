bbcc7974dd93cce8d449f70a167b0097d649f8cf
polirritmico <ebray187@gmail.com>
2025-03-06 18:44:42 -0300

    tests(epub_exporter): user tmp_dir and Path to check outputs

--- tests/test_epub_exporter.py
+++ tests/test_epub_exporter.py
@@ -2,27 +2,41 @@
 # -*- coding: utf-8 -*-
 
 from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import pytest
 
 from src.exporters.epub import EpubExporter
 from src.importers.epub import EpubImporter
 
 
-def test_export_epub() -> None:
+@pytest.fixture
+def tmp_dir():
+    with TemporaryDirectory(dir="tests/files/", prefix="epub_exporter-test-") as tmp:
+        yield Path(tmp)
+
+
+def test_export_epub(tmp_dir) -> None:
     case = [Path("tests/files/simple_ebook.epub")]
-    output_path = "tests/files/outputs/test_export_epub"
+    expected = tmp_dir / "test_export_epub"
 
     importer = EpubImporter()
     exporter = EpubExporter()
     importer.load_data(case)
     document = importer.generate_document()
-    exporter.export(document, output_path)
+    exporter.export(document, expected)
+
+    assert expected.exists()
+    assert expected.is_file()
+    assert expected.stat().st_size > 0
 
 
-def test_update_epub_section() -> None:
+def test_update_epub_section(tmp_dir) -> None:
     case = "THIS IS WORKING"
     expected = "THIS IS WORKING"
+    expected_file = tmp_dir / "test_update_epub_section.epub"
     case_input = [Path("tests/files/simple_ebook.epub")]
-    case_output = [Path("tests/files/outputs/test_update_epub_section.epub")]
+    case_output = [expected_file]
 
     prev_importer = EpubImporter()
     prev_importer.load_data(case_input)
@@ -41,3 +55,7 @@ def test_update_epub_section() -> None:
     output = after_section.content.find("p").string
 
     assert expected in output
+
+    assert expected_file.exists()
+    assert expected_file.is_file()
+    assert expected_file.stat().st_size > 0
