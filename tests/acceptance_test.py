cb63861e9aa8a64d434c03b68f3183ead74720cf
polirritmico <ebray187@gmail.com>
2025-03-06 18:08:50 -0300

    tests(acceptance): use tmp dir instead of manual file handling

--- tests/acceptance_test.py
+++ tests/acceptance_test.py
@@ -15,21 +15,25 @@ os.environ["MIOPEN_LOG_LEVEL"] = "3"
 
 @pytest.fixture
 def tmp_dir():
-    with TemporaryDirectory(dir="tests/files/", prefix="acceptance-test_") as tmp:
-        yield tmp
+    with TemporaryDirectory(dir="tests/files/", prefix="acceptance-test-") as tmp:
+        yield Path(tmp)
+
+
 
 
 # @pytest.mark.skip(reason="Slow execution")
 def test_tts_ebook_to_wav(tmp_dir) -> None:
-    expected_file = Path("tests/files/outputs/tts_test/Chapter 1.wav")
-    expected_file.unlink(missing_ok=True)
+    case = ["Section0001.xhtml"]
+    expected = tmp_dir / "Chapter 1.wav"
+
     word_dict = {
         "Chapter": "Capítulo",
         "This is a basic paragraph.": "Esto es un párrafo sencillo.",
     }
-    case = {
+    opts = {
         "input": "tests/files/simple_ebook.epub",
-        "output": "tests/files/outputs/tts_test/",
+        "output": tmp_dir,
+        "selection": case,
         "transmuter": ("CoquiTTS", "ModelVittsEs"),
         "importer": "EpubImporter",
         "exporter_opts": {
@@ -38,25 +42,26 @@ def test_tts_ebook_to_wav(tmp_dir) -> None:
             "keep_wav": True,
             "word_dict": word_dict,
         },
-        "selection": ["Section0001.xhtml"],
     }
 
     scriptum = Scriptorium()
-    scriptum.setup(case)
+    scriptum.setup(opts)
     scriptum.load_data()
     scriptum.transmute()
     scriptum.export()
 
-    assert expected_file.exists()
-    assert expected_file.is_file()
-    assert expected_file.stat().st_size > 0
+    assert expected.exists()
+    assert expected.is_file()
+    assert expected.stat().st_size > 0
 
 
 # @pytest.mark.skip(reason="Slow execution")
 def test_translate_full_ebook(tmp_dir) -> None:
+    expected = tmp_dir / "output.epub"
     case = {
         "input": "tests/files/simple_ebook.epub",
-        "output": "tests/files/output.epub",
+        "output": expected,
+        "selection": ["Section0001.xhtml"],
         "transmuter": ("OllamaTranslator", ""),
         "importer": "EpubImporter",
         "exporter": "EpubExporter",
@@ -69,17 +74,29 @@ def test_translate_full_ebook(tmp_dir) -> None:
     # scriptum.validate_output()
     scriptum.export()
 
+    assert expected.exists()
+    assert expected.is_file()
+    assert expected.stat().st_size > 0
 
-def test_epub_to_txt() -> None:
-    case = {
+
+def test_epub_to_txt(tmp_dir) -> None:
+    case = ["*"]
+    expected = tmp_dir / "output.txt"
+    opts = {
         "input": "tests/files/simple_ebook.epub",
-        "output": "output.txt",
+        "output": expected,
+        "selection": case,
         "transmuter": ("DummyTransmuter", ""),
         "importer": "EpubImporter",
         "exporter": "SimpleTextExporter",
     }
 
     scriptum = Scriptorium()
-    scriptum.setup(case)
+    scriptum.setup(opts)
     scriptum.load_data()
+    scriptum.transmute()
     scriptum.export()
+
+    assert expected.exists()
+    assert expected.is_file()
+    assert expected.stat().st_size > 0
