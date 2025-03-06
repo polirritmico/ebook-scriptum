33fb45613c5b62d49a37c05dcc3e6059f7b05860
polirritmico <ebray187@gmail.com>
2025-03-06 19:35:01 -0300

    tests(simple_text_exporter): update config types and use tmp_dir

--- tests/test_simple_text_exporter.py
+++ tests/test_simple_text_exporter.py
@@ -3,26 +3,37 @@
 
 
 from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import pytest
 
 from src.scriptorium import Scriptorium
 
 
-def test_simple_text_export() -> None:
-    case_output_path = Path("tests/files/outputs/test_simple_text_export.txt")
+@pytest.fixture
+def tmp_dir():
+    with TemporaryDirectory(dir="tests/files/", prefix="acceptance-test-") as tmp:
+        yield Path(tmp)
+
+
+def test_simple_text_export(tmp_dir) -> None:
+    expected = "domingo"
+    output_file = tmp_dir / "test_simple_text_export.txt"
     case = {
         "input": "tests/files/simple-translation.txt",
-        "output": case_output_path,
+        "output": output_file,
+        "selection": ["*"],
         "transmuter": ("OllamaTranslator", ""),
         "importer": "SimpleTextImporter",
         "exporter": "SimpleTextExporter",
     }
-    expected = "domingo"
 
     scriptum = Scriptorium()
     scriptum.setup(case)
     scriptum.load_data()
     scriptum.transmute()
     scriptum.export()
-    output = case_output_path.read_text()
 
-    assert expected in output.lower()
+    assert output_file.exists()
+    assert output_file.is_file()
+    assert expected in output_file.read_text().lower()
