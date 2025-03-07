5d19ea9eadeb61419be262f7998b1b8e58fb0c06
polirritmico <ebray187@gmail.com>
2025-03-06 21:31:15 -0300

    tests(coqui_tts): add log generation test scope

--- tests/test_coqui_tts.py
+++ tests/test_coqui_tts.py
@@ -21,9 +21,14 @@ def tmp_dir():
 @pytest.mark.filterwarnings("ignore::UserWarning")
 def test_tts_acceptance(tmp_dir) -> None:
     case_file = Path("tests/files/simple.txt")
+    expected_log = tmp_dir / "tts_log"
     expected1 = tmp_dir / "simple.wav"
     expected2 = tmp_dir / "simple.mp3"
-    case_opts = {"text_processor_opts": {"lang": "es", "log": True, "keep_wav": True}}
+    case_opts = {
+        "text_processor_opts": {"lang": "es"},
+        "keep_wav": True,
+        "log": expected_log,
+    }
 
     text = SimpleTextImporter()
     text.load_data(case_file)
@@ -42,3 +47,5 @@ def test_tts_acceptance(tmp_dir) -> None:
     assert expected2.exists()
     assert expected2.is_file()
     assert expected2.stat().st_size > 0, "mp3 file should not be an empty file"
+
+    assert any(expected_log.glob("*.txt")), f"No log files at '{expected_log}'"
