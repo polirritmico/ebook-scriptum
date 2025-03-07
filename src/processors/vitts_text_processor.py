2922a974c58ca83a602ce85a95e405593811e441
polirritmico <ebray187@gmail.com>
2025-03-06 22:06:19 -0300

    feat(text_processor): improve short sentences for tts output.

--- src/processors/vitts_text_processor.py
+++ src/processors/vitts_text_processor.py
@@ -69,17 +69,15 @@ class VittsTextProcessor:
         lines_to_remove = []
         for line_number, line in enumerate(text):
             if prev_line:
-                # TODO: Check the impact of this:
-                prev_line = prev_line.replace(".", " .")
-                # prev_line = prev_line.replace(".", ",")
-
                 line = prev_line + " " + line
                 text[line_number] = line
                 prev_line = None
 
             line_width = len(line)
             if line_width <= min_width:
-                prev_line = line
+                # TODO: Check the impact of this:
+                prev_line = line.replace(".", " .")
+                # prev_line = prev_line.replace(".", ",")
                 lines_to_remove.append(line_number)
 
         if prev_line:
